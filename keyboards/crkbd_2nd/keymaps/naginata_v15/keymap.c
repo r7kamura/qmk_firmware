6ac92ecb1c233c4049cab253d4c86f2e6cd48d53
r7kamura <r7kamura@gmail.com>
2024-04-22 18:06:25 +0900

    Update keymap

--- keyboards/crkbd_2nd/keymaps/naginata_v15/keymap.c
+++ keyboards/crkbd_2nd/keymaps/naginata_v15/keymap.c
@@ -33,8 +33,8 @@ enum custom_keycodes {
   KANA2,
 };
 
-#define ALTEISU LALT_T(EISU)
-#define ALTKANA RALT_T(KANA2)
+#define CTLEISU LCTL_T(EISU)
+#define CTLKANA RCTL_T(KANA2)
 #define MOLOWER MO(_LOWER)
 #define MORAISE MO(_RAISE)
 #define SFTENT RSFT_T(KC_ENT)
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_END,    KC_PGDN,    KC_H,    KC_J,    KC_K,    KC_L, JP_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, JP_COMM,  JP_DOT, JP_SLSH, KC_RWIN,
+      KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, JP_COMM,  JP_DOT, JP_SLSH, KC_RWIN,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                          MOLOWER, ALTEISU,  SFTSPC,     SFTENT, ALTKANA, MOLOWER
+                                          MOLOWER, CTLEISU,  SFTSPC,     SFTENT, CTLKANA, MOLOWER
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -59,21 +59,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
        KC_TAB,    NG_A,    NG_S,    NG_D,    NG_F,    NG_G,  KC_END,    KC_PGDN,    NG_H,    NG_J,    NG_K,    NG_L, NG_SCLN, _______,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    NG_Z,    NG_X,    NG_C,    NG_V,    NG_B,                         NG_N,    NG_M, NG_COMM,  NG_DOT, NG_SLSH, KC_RWIN,
+      KC_LALT,    NG_Z,    NG_X,    NG_C,    NG_V,    NG_B,                         NG_N,    NG_M, NG_COMM,  NG_DOT, NG_SLSH, KC_RWIN,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                          MOLOWER, ALTEISU,  SFTSPC,     SFTENT, ALTKANA, MOLOWER
+                                          MOLOWER, CTLEISU,  SFTSPC,     SFTENT, CTLKANA, MOLOWER
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-      JP_EXLM, JP_PERC,    KC_7,    KC_8,    KC_9, JP_MINS, _______,    _______,   JP_AT, JP_HASH,   KC_UP, JP_LCBR, JP_RCBR,  KC_DEL,
+       JP_DLR, JP_PERC,    KC_7,    KC_8,    KC_9, JP_MINS,   KC_F4,    KC_VOLD,   JP_AT, JP_HASH,   KC_UP, JP_LCBR, JP_RCBR,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      JP_CIRC, JP_ASTR,    KC_4,    KC_5,    KC_6, JP_PLUS, _______,    _______, JP_TILD, KC_LEFT, KC_DOWN,KC_RIGHT, JP_LPRN, JP_RPRN,
+      JP_CIRC, JP_ASTR,    KC_4,    KC_5,    KC_6, JP_PLUS,  KC_F11,    KC_VOLU, JP_UNDS, KC_LEFT, KC_DOWN,KC_RIGHT, JP_LBRC, JP_RBRC,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-       JP_DLR,    KC_0,    KC_1,    KC_2,    KC_3,  JP_EQL,                      JP_AMPR, JP_PIPE,  JP_GRV, JP_UNDS, JP_LBRC, JP_RBRC,
+      KC_LALT,    KC_0,    KC_1,    KC_2,    KC_3,  JP_EQL,                      JP_AMPR, JP_LPRN, JP_RPRN, JP_BSLS, JP_EXLM, KC_RWIN,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                          _______, CTLEISU,  SFTSPC,     SFTENT, CTLKANA, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
